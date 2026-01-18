121f49f4e1c214d8a887340632d4d3d0ea98a414
Kinza Qureshi <kinza@Kinzas-MacBook-Air.local>
2026-01-18 21:36:04 +0000

    Main settings

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,25 @@
 """
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+URL configuration for ShopClub project.
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', include('products.urls')),
+    path('orders/', include('orders.urls')),
+    path('user/', include('accounts.urls')),
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Customize admin site
+admin.site.site_header = "ShopClub Admin"
+admin.site.site_title = "ShopClub Admin Portal"
+admin.site.index_title = "Welcome to ShopClub Administration"
